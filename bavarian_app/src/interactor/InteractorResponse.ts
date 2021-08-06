ac3e223e6583547040376165ec8a282f66aa9571
Patrick Frömel <froemel>
2021-08-06 11:26:48 +0200

    First implementation of the Interactor

---
+++ bavarian_app/src/interactor/InteractorResponse.ts
@@ -0,0 +1,49 @@
+import {Category} from "../entities/Category";
+import {VocabularyWord} from "../entities/VocabularyWord";
+import {Quiz} from "../entities/Quiz";
+
+export interface InteractorResponseCategories {
+
+  /**
+   * Called as soon as the request of all categories is finished
+   * @param cats
+   */
+  respondAllCategories(cats:Category[]):void;
+
+}
+
+export interface InteractorResponseStudy {
+
+  /**
+   * Called as soon as the request of a spezific study is finished
+   * @param study
+   */
+  respondStudy(study:VocabularyWord[]):void;
+
+}
+
+export interface InteractorResponseQuiz {
+
+  /**
+   * Called as soon as the request of a spezific quiz is finished
+   * @param quiz
+   */
+  respondQuiz(quiz:Quiz):void;
+
+}
+
+export interface InteractorResponseProgress {
+
+  /**
+   * Called as soon as the request of the progress of a category is completed
+   * @param progress
+   */
+  respondProgress(progress:number);
+
+  /**
+   * Called as soon as the request of the progress of all categories is completed
+   * @param progress
+   */
+  respondProgress(progress:Map<Category,number>)
+
+}
