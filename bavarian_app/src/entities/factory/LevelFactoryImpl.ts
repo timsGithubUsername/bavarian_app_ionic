563a273dd9be436386b9772f0ea82fb834562eec
Patrick Frömel <froemel>
2021-08-07 09:50:30 +0200

    LevelFactory implemented

---
+++ bavarian_app/src/entities/factory/LevelFactoryImpl.ts
@@ -0,0 +1,54 @@
+import {LevelFactory} from "./LevelFactory.js";
+import {LevelMutable} from "../Level.js";
+
+export class LevelFactoryImpl implements LevelFactory{
+
+  createLevel(id: number, iconPath: string): LevelMutable {
+    return new LevelImpl(id,iconPath);
+  }
+
+}
+
+class LevelImpl implements LevelMutable {
+
+  private id:number;
+  private iconPath:string;
+
+  constructor(id:number,iconPath:string) {
+    this.setId(id);
+    this.setIconPath(iconPath);
+  }
+
+
+  /**
+   * Returns the path of the icon of the level
+   */
+  getIconPath(): string {
+    return this.iconPath;
+  }
+
+  /**
+   * Returns the number of the level
+   */
+  getId(): number {
+    return this.id;
+  }
+
+  /**
+   * Sets the path of the icon of the level
+   * @param path
+   */
+  setIconPath(path: string): void {
+    this.iconPath = path;
+  }
+
+
+  /**
+   * Sets the number of the level
+   * @param id
+   */
+  setId(id: number): void {
+    this.id = id;
+  }
+
+}
