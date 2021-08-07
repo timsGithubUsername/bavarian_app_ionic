f12cb8ef0486d50ffa791230831c6a3983b93a65
mauxen <spamham1@gmx.de>
2021-08-07 16:18:58 +0200

    implemented categories[] -> map<Level, categories[]> in ResponseCategoriesImpl

--- bavarian_app/src/app/controller/ResponseCategoriesImpl.ts
+++ bavarian_app/src/app/controller/ResponseCategoriesImpl.ts
@@ -1,8 +1,36 @@
 import {InteractorResponseCategories} from "../../interactor/InteractorResponse";
 import {Category} from "../../entities/Category";
+import {Level} from "../../entities/Level";
 
 export class ResponseCategoriesImpl implements InteractorResponseCategories{
+  private categoryLevelMap: Map<Level, Category[]>;
+
   respondAllCategories(cats: Category[]): void {
+    this.buildCategoryLevelMap(cats);
   }
 
+  /**
+   * Build the Map over level to Categories with given Categories-Array
+   * @param cats The given Array
+   * @private respect my privacy
+   */
+  private buildCategoryLevelMap(cats: Category[]){
+    //stores the current level
+    let currentLevel: Level;
+
+    //for each category..
+    cats.forEach(element => {
+      //.. set currentLevel..
+      currentLevel = element.getLevel();
+
+      //.. if level already exists..
+      if(this.categoryLevelMap.has(currentLevel)){
+        //.. add element to array for this level, else..
+        this.categoryLevelMap.get(currentLevel).push(element);
+      } else {
+        //.. set new key-value-pair
+        this.categoryLevelMap.set(currentLevel, [element]);
+      }
+    });
+  }
 }
