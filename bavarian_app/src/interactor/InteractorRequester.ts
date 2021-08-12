0a12b9351905ee463ec7812aa404f1ec6b7c59e8
Patrick Frömel <froemel>
2021-08-12 09:18:35 +0200

    Interactor has been adapted accordingly

--- bavarian_app/src/interactor/InteractorRequester.ts
+++ bavarian_app/src/interactor/InteractorRequester.ts
@@ -1,17 +1,46 @@
 import {Category} from "../entities/Category";
 import {VocabularyWord} from "../entities/VocabularyWord";
 import {Quiz} from "../entities/Quiz";
+import {Level} from "../entities/Level";
+import {Language} from "../entities/Language";
+import {Dialect} from "../entities/Dialect";
 
 export interface InteractorRequester {
 
   /**
-   * Requests all categories in the database
-   * @param response Called as soon as the request of a spezific study is finished
+   * Requests all Languages of the Database
+   * @param response Called as soon as the request ist finished
    */
-  requestAllCategories(response:((cats:Category[]) => void)):void;
+  requestAllLanguages(response:((langs:Language[]) => void)):void;
+
+  /**
+   * Requests all Dialects of the Database
+   * @param response Called as soon as the request ist finished
+   */
+  requestAllDialects(response:((dialects:Dialect[]) => void)):void;
+
+  /**
+   * Sets the language for all upcoming requests
+   * @param language
+   */
+  setLanguage(language:Language):void;
+
+  /**
+   * Sets the dialect for all upcoming requests
+   * @param dialect
+   */
+  setDialect(dialect:Dialect):void;
+
+  /**
+   * Requests all levels in the database
+   * Language and dialect must be set before
+   * @param response Called as soon as the request of all levels is finished
+   */
+  requestAllLevels(response:((levels:Level[]) => void)):void;
 
   /**
    * Requests the study for a category
+   * Language and dialect must be set before
    * @param cat
    * @param response Called as soon as the request of a spezific study is finished
    */
@@ -19,6 +48,7 @@ export interface InteractorRequester {
 
   /**
    * Requests a quiz for a category
+   * Language and dialect must be set before
    * @param cat
    * @param response Called as soon as the request of a spezific quiz is finished
    */
@@ -26,6 +56,7 @@ export interface InteractorRequester {
 
   /**
    * Requests progress in a category
+   * Language and dialect must be set before
    * @param cat
    * @param type Indicates which progress is to be returned
    * @param response Called as soon as the request of the progress of a category is completed
@@ -34,6 +65,7 @@ export interface InteractorRequester {
 
   /**
    * Requests progress in all categories
+   * Language and dialect must be set before
    * @param type Indicates which progress is to be returned
    * @param response Called as soon as the request of the progress of all categories is completed
    */
@@ -41,6 +73,7 @@ export interface InteractorRequester {
 
   /**
    * Stores a new progress value for a certain category
+   * Language and dialect must be set before
    * @param cat - Category for which the progress needs to be saved
    * @param type Indicates which progress is to be saved
    * @param value - New progress to be saved
